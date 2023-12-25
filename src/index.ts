31a132e942d8470752f37adfc1e2e281625ae25a
David-Nezri <davidnezri1990@gmail.com>
2023-12-25 12:44:25 +0200

    added helpers

---
+++ src/index.ts
@@ -0,0 +1,93 @@
+export {
+  IAuthPayload,
+  IAuthDocument,
+  IAuthBuyerMessageDetails,
+  IEmailMessageDetails,
+  IAuth,
+  ISignUpPayload,
+  ISignInPayload,
+  IForgotPassword,
+  IResetPassword,
+  IReduxAuthPayload,
+  IReduxAddAuthUser,
+  IReduxLogout,
+  IAuthResponse,
+  IAuthUser,
+} from './interface/auth.interface';
+export { IBuyerDocument, IReduxBuyer } from './interface/buyer.interface';
+export {
+  IConversationDocument,
+  IMessageDocument,
+  IMessageDetails,
+  IChatBoxProps,
+  IChatSellerProps,
+  IChatBuyerProps,
+  IChatMessageProps,
+} from './interface/chat.interface';
+export {
+  GigType,
+  ICreateGig,
+  ISellerGig,
+  IGigContext,
+  IGigsProps,
+  IGigCardItems,
+  ISelectedBudget,
+  IGigViewReviewsProps,
+  IGigInfo,
+  IGigTopProps,
+} from './interface/gig.interface';
+export {
+  IOrderNotifcation,
+  IOffer,
+  IExtendedDelivery,
+  IDeliveredWork,
+  IOrderEvents,
+  IOrderReview,
+  IOrderMessage,
+  IOrderDocument,
+} from './interface/order.interface';
+export {
+  IReviewMessageDetails,
+  IRatingTypes,
+  IRatingCategories,
+  IRatingCategoryItem,
+  IReviewDocument,
+} from './interface/review.interface';
+export {
+  ISearchResult,
+  IHitsTotal,
+  IQueryList,
+  IQueryString,
+  ITerm,
+  IPaginateProps,
+} from './interface/search.interface';
+export {
+  SellerType,
+  ILanguage,
+  IExperience,
+  IEducation,
+  ICertificate,
+  ISellerDocument,
+} from './interface/seller.interface';
+export { IEmailLocals } from './interface/email.interface';
+export { uploads, videoUpload } from './cloudinary-upload';
+export {
+  IErrorResponse,
+  IError,
+  CustomError,
+  BadRequestError,
+  NotFoundError,
+  NotAuthorizedError,
+  FileTooLargeError,
+  ServerError,
+  ErrnoException
+} from './error-handler';
+export { verifyGatewayRequest } from './gateway-middleware';
+export { winstonLogger } from './logger';
+export {
+  firstLetterUppercase,
+  lowerCase,
+  toUpperCase,
+  isEmail,
+  isDataURL
+} from './helpers';
